c1b9e1ebda06e4ede504333e9f22dfcb5ed434e2
Gilnei Nogueira <gilnei.nogueira@live.com>
2021-02-23 15:53:33 -0300

    Add project files.

---
+++ CompanyManager/Models/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyManager.Models
+{
+    public class Funcionario
+    {
+        public string NomeCompleto { get; set; }
+        public string Email { get; set; }
+        public int NumeroDeChapa { get; set; }
+        public int Ddd { get; set; }
+        public int Telefone { get; set; }
+
+    }
+}
