a5121fca3aed2f4c9105272826089eab401115d7
Gilnei Nogueira <gilnei.nogueira@live.com>
2021-02-23 20:28:49 -0300

    Adicionando Models e Services

--- CompanyManager/Models/Funcionario.cs
+++ CompanyManager/Models/Funcionario.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,17 @@ namespace CompanyManager.Models
 {
     public class Funcionario
     {
-        public string NomeCompleto { get; set; }
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
         public string Email { get; set; }
         public int NumeroDeChapa { get; set; }
-        public int Ddd { get; set; }
-        public int Telefone { get; set; }
-
+        public int? Ddd1 { get; set; }
+        public int? Telefone1 { get; set; }
+        public int? Ddd2 { get; set; }
+        public int? Telefone2 { get; set; }
+        public Guid LiderId { get; set; }
+        public string Senha { get; set; }
+        public string Login { get; set; }
     }
 }
---
+++ CompanyManager/Models/Lider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyManager.Models
+{
+    public class Lider
+    {
+        public Guid Id { get; set; }
+        public Guid FuncionarioId { get; set; }
+
+    }
+}
